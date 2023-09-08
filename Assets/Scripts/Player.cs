9ff20ce25642119e0ed7caee1f4cdebcb33841d8
Miguel Perez <miguepersa@gmail.com>
2023-09-07 20:28:17 -0400

    Collector

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -98,4 +98,9 @@ public class Player : MonoBehaviour
 
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(ENEMY_TAG)) Destroy(gameObject);
+    }
+
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -29,24 +29,28 @@ public class Spawner : MonoBehaviour
 
     IEnumerator SpawnMonsters()
     {
-        yield return new WaitForSeconds(Random.Range(1, 5));
-
-        randomIndex = Random.Range(0, monsterReference.Length);
-        randomSide = Random.Range(0, 2);
-
-        spawnedMonster = Instantiate(monsterReference[randomIndex]);
-
-        if (randomSide == 0)
+        while (true)
         {
-            spawnedMonster.transform.position = leftPos.position;
-            spawnedMonster.GetComponent<Enemy>().speed = Random.Range(4, 10);
+            yield return new WaitForSeconds(Random.Range(1, 5));
+
+            randomIndex = Random.Range(0, monsterReference.Length);
+            randomSide = Random.Range(0, 2);
+
+            spawnedMonster = Instantiate(monsterReference[randomIndex]);
+
+            if (randomSide == 0)
+            {
+                spawnedMonster.transform.position = leftPos.position;
+                spawnedMonster.GetComponent<Enemy>().speed = Random.Range(4, 10);
+            }
+            else
+            {
+                spawnedMonster.transform.position = rightPos.position;
+                spawnedMonster.GetComponent<Enemy>().speed = -Random.Range(4, 10);
+                spawnedMonster.transform.localScale = new Vector3(-1f, 1f, 1f);
+            }
         }
-        else
-        {
-            spawnedMonster.transform.position = rightPos.position;
-            spawnedMonster.GetComponent<Enemy>().speed = -Random.Range(4, 10);
-            spawnedMonster.transform.localScale = new Vector3(-1f, 1f, 1f);
-        }
+            
 
     }
 }
